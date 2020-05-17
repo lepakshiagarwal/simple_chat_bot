c9f61a8a7d5db74b225f659dbaa5d59ed6dc65d5
lepakshiagarwal <60332064+lepakshiagarwal@users.noreply.github.com>
2020-05-17 17:41:20 -0400

    Counter implementation

--- Project.py
+++ Project.py
@@ -10,3 +10,11 @@ remainder_of_5 = int(input())
 remainder_of_7 = int(input())
 age = (remainder_of_3 * 70 + remainder_of_5 * 21 + remainder_of_7 * 15) % 105
 print("Your age is"+ str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+number = int(input())
+counter = 0
+while(counter <= number):
+    print(str(counter) + "!")
+    counter = counter + 1
+
+print('Completed, have a nice day!')
