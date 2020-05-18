ca66605a54f96f7d5e0a38e03977779f496ab842
lepakshiagarwal <60332064+lepakshiagarwal@users.noreply.github.com>
2020-05-18 10:11:16 -0400

    Functions and quiz feature implemented

--- Project.py
+++ Project.py
@@ -1,20 +1,59 @@
-print("Hello! My name is Aid.")
-print("I was created in 2020.")
-print('Please, remind me your name.')
-your_name = str(input())
-print('What a great name you have,'+ your_name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
-remainder_of_3 = int(input())
-remainder_of_5 = int(input())
-remainder_of_7 = int(input())
-age = (remainder_of_3 * 70 + remainder_of_5 * 21 + remainder_of_7 * 15) % 105
-print("Your age is"+ str(age) + "; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
-number = int(input())
-counter = 0
-while(counter <= number):
-    print(str(counter) + "!")
-    counter = counter + 1
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-print('Completed, have a nice day!')
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print("Why do we use methods?")
+    print("1. To repeat a statement multiple times.")
+    print("2. To decompose a program into several small subroutines.")
+    print("3. To determine the execution time of a program.")
+    print("4. To interrupt the execution of a program.")
+    while True:
+        answer = int(input())
+        if answer != 2:
+            print("Please, try again.")
+        else:
+            break
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  
+remind_name()
+guess_age()
+count()
+test()
+end()
